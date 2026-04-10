ad19e54f837f4bcc45a8692b3224d8c690b7be36
Artem Tyshchuk <artemtyschukk@gmail.com>
2026-04-10 08:41:10 +0200

    Add mobile video support in SectionDivider; implement responsive video loading based on screen size

--- client/src/features/Home/SectionDivider.tsx
+++ client/src/features/Home/SectionDivider.tsx
@@ -1,30 +1,78 @@
 "use client";
 import { LazyVideo } from "lazyvid";
+import { useEffect, useState } from "react";
 
 export const SectionDivider = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const media = window.matchMedia("(max-width: 999px)");
+    const onChange = () => setIsMobile(media.matches);
+
+    onChange();
+    media.addEventListener("change", onChange);
+
+    return () => media.removeEventListener("change", onChange);
+  }, []);
+
   return (
     <section className=" mx-[-16px] lg:mx-[-40px] w-calc(100% + 80px) h-auto">
-      <div className="w-full h-[478px] overflow-hidden">
-        <LazyVideo
-          sources={[
-            {
-              src: "/videos/videoDivider/videoDivider.webm",
-              type: "video/webm",
-            },
-            { src: "/videos/videoDivider/videoDivider.mp4", type: "video/mp4" },
-          ]}
-          className="w-full h-full object-cover scale-120"
-          autoPlay
-          muted
-          controls={false}
-          loop
-          playsInline
-          preload="auto"
-          poster="/images/homeDivider.jpg"
-          pauseOnLeave
-          onError={() => {}}
-        />
-      </div>
+      {isMobile ? (
+        <div className="w-full h-[500px] overflow-hidden">
+          <VideoDivider
+            sources={[
+              {
+                src: "/videos/videoDivider/mobile_videoDivider.webm",
+                type: "video/webm",
+              },
+              {
+                src: "/videos/videoDivider/mobile_videoDivider.mp4",
+                type: "video/mp4",
+              },
+            ]}
+          />
+        </div>
+      ) : (
+        <div className="w-full h-[478px] overflow-hidden">
+          <VideoDivider
+            sources={[
+              {
+                src: "/videos/videoDivider/videoDivider.webm",
+                type: "video/webm",
+              },
+              {
+                src: "/videos/videoDivider/videoDivider.mp4",
+                type: "video/mp4",
+              },
+            ]}
+          />
+        </div>
+      )}
     </section>
+  );
+};
+
+const VideoDivider = ({
+  sources,
+}: {
+  sources: { src: string; type: string }[];
+}) => {
+  return (
+    <>
+      <LazyVideo
+        sources={sources}
+        className="w-full h-full object-cover"
+        autoPlay
+        muted
+        controls={false}
+        loop
+        playsInline
+        preload="auto"
+        poster="/images/homeDivider.jpg"
+        pauseOnLeave
+        onError={() => {}}
+        rootMargin="700px"
+      />
+    </>
   );
 };
